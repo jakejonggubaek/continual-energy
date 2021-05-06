bb2afa657e54b85309d6057e81c89243d0f24911
jakejonggubaek <jakejonggubaek@gmail.com>
2021-05-06 00:19:46 -0400

    added responsive UI

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const valueContainer = document.querySelectorAll('.value-container');
 const valueContents = document.querySelectorAll('.value-content');
 const valueImage = document.querySelectorAll('.value-image');
 const valueTitle = document.querySelectorAll('.value-title');
+const loginBtn = document.querySelector('.login-form-open');
+const loginForm = document.querySelector('.login-form');
+const closeBtn = document.querySelector('.close-button');
+
+loginBtn.addEventListener('click', () => {
+    loginForm.style.visibility = 'visible';
+});
+
+closeBtn.addEventListener('click', () => {
+    loginForm.style.visibility = 'hidden';
+});
+
 
 const toggleItems = (index) => {
     valueImage[index].classList.toggle("hidden");
